4a00abe5b87fa412d2577818f3eaf3a6420b867a
Sristi <sristikundu2468@gmail.com>
2026-03-03 01:57:21 +0600

    create the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
 import { toast, ToastContainer } from "react-toastify";
 import Navbar from "./Components/Navbar/Navbar";
 import Banner from "./Components/Banner/Banner";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
-  
-//   const handleclick = () =>{
+  //   const handleclick = () =>{
 
-// toast("Wow so easy!", {
-// position: "top-right",
-// autoClose: 5000,
-// hideProgressBar: false,
-// closeOnClick: false,
-// pauseOnHover: true,
-// draggable: true,
-// progress: undefined,
-// theme: "light"
-// });
-//   }
+  // toast("Wow so easy!", {
+  // position: "top-right",
+  // autoClose: 5000,
+  // hideProgressBar: false,
+  // closeOnClick: false,
+  // pauseOnHover: true,
+  // draggable: true,
+  // progress: undefined,
+  // theme: "light"
+  // });
+  //   }
 
   return (
     <>
-
-    {/* <ToastContainer/> */}
-    <Navbar></Navbar>
-    <Banner></Banner>
-     
+      {/* <ToastContainer/> */}
+      <Navbar></Navbar>
+      <Banner></Banner>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <footer className="footer footer-horizontal gap-4 footer-center bg-base-200 text-base-content rounded p-10 py-20">
+      <nav>
+        <a className=" text-xl text-[#003EA4]">
+          Auction <span className="text-[#FFD337]">Gallery</span>
+        </a>
+      </nav>
+      <nav className="grid grid-flow-col gap-4 text-black text-lg font-normal leading-7">
+        <a className="link link-hover">Bid. </a>
+        <a className="link link-hover">Win.</a>
+        <a className="link link-hover">Own.</a>
+      </nav>
+      <nav className="grid grid-flow-col gap-4 text-black text-sm font-normal leading-6">
+        <a className="link link-hover">Home </a>
+        <a className="link link-hover">Auctions </a>
+        <a className="link link-hover">Categories </a>
+        <a className="link link-hover">How to works</a>
+      </nav>
+      <aside>
+        <p className="text-black text-sm font-normal leading-6">
+          © 2025 AuctionHub. All rights reserved.
+        </p>
+      </aside>
+    </footer>
+  );
 };
 
 export default Footer;
